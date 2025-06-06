3f09a2a73a5bc9c62d8272e5e76295e67778f7b7
Gaurav Dwivedi <gauravdwivedi@Gauravs-MacBook-Air.local>
2025-06-06 12:30:24 +0530

    links

--- src/components/BottomLink.jsx
+++ src/components/BottomLink.jsx
@@ -6,15 +6,16 @@ import theme from '../hooks/theme';
 const BottomLink = ({text, link}) => {
   const {setableTheme, settingTheme} = theme();
 
+  const hoverTextColor = setableTheme === 'dark' ? 'text-gray-500' : 'text-black';
   const hoverUnderlineColor = setableTheme === 'dark' ? 'bg-gray-500' : 'bg-black';
 
   return (
     <p className='text-[13px] mt-17 mb-8 flex gap-10 items-center'>
     <NavLink to={link} className="relative group text-gray-600">
-      <span className={`group-hover:${hoverUnderlineColor} transition-colors duration-300`}>
+      <span className={`group-hover:${hoverTextColor} transition-colors duration-300`}>
         {text}
       </span>
-      <span className="absolute left-0 -bottom-1 w-full h-[1px] bg-black scale-x-0 origin-center transition-transform duration-300 group-hover:scale-x-100"></span>
+      <span className={`absolute left-0 -bottom-1 w-full h-[1px] ${hoverUnderlineColor} scale-x-0 origin-center transition-transform duration-300 group-hover:scale-x-100`}></span>
     </NavLink>
     <BsArrowRight className='text-2xl font-light animate-ping' />
   </p>
