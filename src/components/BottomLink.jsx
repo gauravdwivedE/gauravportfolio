d1bce141081d02c3bd2fdff62c189134f0cd9b9b
Gaurav Dwivedi <gauravdwivedi@Gauravs-MacBook-Air.local>
2025-06-06 12:28:31 +0530

    links

--- src/components/BottomLink.jsx
+++ src/components/BottomLink.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { BsArrowRight } from 'react-icons/bs';
 import { NavLink } from 'react-router-dom';
+import theme from '../hooks/theme';
 
 const BottomLink = ({text, link}) => {
+  const {setableTheme, settingTheme} = theme();
+
+  const hoverUnderlineColor = setableTheme === 'dark' ? 'bg-gray-500' : 'bg-black';
+
   return (
     <p className='text-[13px] mt-17 mb-8 flex gap-10 items-center'>
     <NavLink to={link} className="relative group text-gray-600">
-      <span className="transition-colors duration-300">
+      <span className={`group-hover:${hoverUnderlineColor} transition-colors duration-300`}>
         {text}
       </span>
       <span className="absolute left-0 -bottom-1 w-full h-[1px] bg-black scale-x-0 origin-center transition-transform duration-300 group-hover:scale-x-100"></span>
